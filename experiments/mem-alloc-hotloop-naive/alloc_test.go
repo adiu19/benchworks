e91887cd9715f19bcae785f83522664f67424118
adiu19 <aditupadhyaya@gmail.com>
2025-11-16 20:13:02 -0800

    lint

--- experiments/mem-alloc-hotloop-naive/alloc_test.go
+++ experiments/mem-alloc-hotloop-naive/alloc_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func BenchmarkAlloc(b *testing.B) {
-    for i := 0; i < b.N; i++ {
-        buf := make([]byte, 1024 * 1024)
-        _ = buf
-    }
+	for i := 0; i < b.N; i++ {
+		buf := make([]byte, 1024*1024)
+		_ = buf
+	}
 }
